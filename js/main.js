45f8977e5f6c26191a49defdcab4bde3982a9653
bobbyfromgeminus <73587718+bobbyfromgeminus@users.noreply.github.com>
2021-03-07 11:08:15 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
 'use strict';
 
-const sendButton = $('#send');
+const sendButton = $('#send');          // natív javascriptben: const sendButton = document.querySelector('#send');
 
-sendButton.click( () => {    
+sendButton.click( () => {               // natív javascriptben: sendButton.addeventListener('click', ()=>{...});
 
-    let name = $('#name').val();
-    let email = $('#email').val();
-    let subject = $('#subject').val();
-    let message = $('#message').val();
+    let name = $('#name').val();        // natív javascriptben: let name = document.querySelector('#name').value;
+    let email = $('#email').val();      // natív javascriptben: let email = document.querySelector('#email').value;
+    let subject = $('#subject').val();  // natív javascriptben: let subject = document.querySelector('#subject').value;
+    let message = $('#message').val();  // natív javascriptben: let message = document.querySelector('#message').innerHTML;
 
-    $.ajax({
+    $.ajax({                            // ajax kérés jQuery módra
             url: 'sendmail.php', 
             method: 'POST',
             data: { name: name, email: email, subject: subject, message: message },
             dataType: 'html',
         }).done( function(data) {
-            document.querySelector('.result').textContent = data;
+            $('.result').html(data);    // natív javascriptben: document.querySelector('.result').innerHTML = data;
         });
 });
